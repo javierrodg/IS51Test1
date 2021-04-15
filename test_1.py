fb9034f9262402f32108fc71899d5698d578f3b8
Javier Rodriguez <javiersmac@Javierss-Mac-mini.local>
2021-04-15 14:36:15 -0700

    finished structured English

---
+++ test_1.py
@@ -0,0 +1,16 @@
+
+"""
+This program is determining which payment option is better.
+The first option is $100 per day for 10 days.
+The second option is $1 dollar a day with it being dopubled each day for 10 days.
+Two functions will be used to calcluate the pay rate.
+Function 1 will calculate the amount for the first option, function 2 will calcluate the amount for the second option.
+
+Function 1 will output 100 * 10 days.
+Function 2 will loop 10 times, with each time doubling the amount while adding the amount to the total.
+
+If option 1 is better it will output that option 1 is better.
+If option 2 is better it will outout that option 2 is better.
+
+"""
+
