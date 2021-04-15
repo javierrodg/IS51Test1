81c1829d5f2c746ead0fdad66a37f49ce2e3a5a4
Javier Rodriguez <javiersmac@Javierss-Mac-mini.local>
2021-04-15 14:45:25 -0700

    finished pseudocode

--- test_1.py
+++ test_1.py
@@ -14,3 +14,30 @@ If option 2 is better it will outout that option 2 is better.
 
 """
 
+"""
+option 1
+    return 100 * 1
+
+option 2 
+amount = 1
+list1 = []
+    loop 10 times
+    add amount to list1
+    amount *= 2
+    sum = sum of all items in loop
+    return sum
+
+main
+var 1 = option1
+var2 = option2
+
+if var1 = var2
+    "option 1 and option 2 pays the same"
+if var1 < var2
+    "option 2 is better" 
+else
+    "Option 1 is better"
+
+main
+
+"""
